4111e5d8233fbdaec873874e7fb4037043d291a9
Maciej Wedzik <logmaciej@gmail.com>
2025-04-21 12:09:37 +0200

    Clean up: ignore build artifacts and IDE files

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maciej/CLionProjects/sbh_instance_gen
-# Build directory: /home/maciej/CLionProjects/sbh_instance_gen/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
